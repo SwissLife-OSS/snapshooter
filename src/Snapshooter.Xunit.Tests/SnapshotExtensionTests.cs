c28319b45e2dedcc22ad6ae588f1f75770dd6fe0
Florian Rohrer <florian.rohrer@vivaldi.net>
2019-05-29 21:03:37 +0200

    Add Test

---
+++ src/Snapshooter.Xunit.Tests/SnapshotExtensionTests.cs
@@ -0,0 +1,20 @@
+using Xunit;
+using FluentAssertions;
+using Snapshooter.Tests.Data;
+
+
+namespace Snapshooter.Xunit.Tests
+{
+    public class SnapshotExtensionTests
+    {
+        [Fact]
+        public void MatchSnapshot_ShouldFluentAssertions_RemovesSubject()
+        {
+            // arrange
+            TestPerson testPerson = TestDataBuilder.TestPersonMarkWalton().Build();
+
+            // act & assert
+            testPerson.Should().MatchSnapshot();
+        }
+    }
+}
