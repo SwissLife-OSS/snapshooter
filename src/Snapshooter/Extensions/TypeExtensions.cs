31cf1fc87fb171a67c3c19671372f279741425b1
Normen Scheiber <nscheibe@live.com>
2020-01-10 16:33:16 +0100

    Added comments to public classes and methods.

--- src/Snapshooter/Extensions/TypeExtensions.cs
+++ src/Snapshooter/Extensions/TypeExtensions.cs
@@ -3,8 +3,16 @@ using System.Collections.Generic;
 
 namespace Snapshooter.Core
 {
+    /// <summary>
+    /// Some extensions for Type, to support snapshot testing.
+    /// </summary>
     public static class TypeExtensions
     {
+        /// <summary>
+        /// Returns the list of inherited types.
+        /// </summary>
+        /// <param name="type">The current object type.</param>
+        /// <returns>The list of all inherited types.</returns>
         public static IEnumerable<Type> BaseTypesAndSelf(this Type type)
         {
             while (type != null)
