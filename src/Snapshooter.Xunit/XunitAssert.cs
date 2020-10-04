fb4707c4fad861bd321b0a5e5e8f4891bf567c69
Florian Rohrer <Florian.Rohrer@vivaldi.net>
2020-10-04 18:20:55 +0200

    add possible solution using message in assert.True

--- src/Snapshooter.Xunit/XunitAssert.cs
+++ src/Snapshooter.Xunit/XunitAssert.cs
@@ -1,4 +1,7 @@
-﻿using Snapshooter.Core;
+﻿using System;
+using System.Linq;
+using Snapshooter.Core;
+
 using x = Xunit;
 
 namespace Snapshooter.Xunit
@@ -9,14 +12,23 @@ namespace Snapshooter.Xunit
     public class XunitAssert : IAssert
     {
         /// <summary>
-        /// Asserts the expected snapshot and the actual snapshot 
+        /// Asserts the expected snapshot and the actual snapshot
         /// with the XUnit assert utility.
         /// </summary>
         /// <param name="expectedSnapshot">The expected snapshot.</param>
         /// <param name="actualSnapshot">The actual snapshot.</param>
         public void Assert(string expectedSnapshot, string actualSnapshot)
         {
-            x.Assert.Equal(expectedSnapshot, actualSnapshot);
+            string[] expectedData = expectedSnapshot.Split('\n');
+            string[] actualData = actualSnapshot.Split('\n');
+
+            for (int i = 0; i < Math.Max(expectedData.Length, actualData.Length); i++)
+            {
+                string expected = expectedData.ElementAtOrDefault(i);
+                string actual = actualData.ElementAtOrDefault(i);
+                x.Assert.True(expected.Equals(actual),
+                    $"Missmatch at line {i + 1}\n\t\tExpected:\t{expected}\n\t\tActual:\t\t{actual}");
+            }
         }
     }
 }
