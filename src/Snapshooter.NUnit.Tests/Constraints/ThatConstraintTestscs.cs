c8f0a07ba7b4878ccbb2b75f64aa76a6e0b3616b
Normen Scheiber <nscheibe@live.com>
2021-01-15 16:40:45 +0100

    Draft nunit that.

---
+++ src/Snapshooter.NUnit.Tests/Constraints/ThatConstraintTestscs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace Snapshooter.NUnit.Tests.Constraints
+{
+    public class ThatConstraintTestscs
+    {
+        [Test]
+        public void TestIfStoredSnapshotIsMatching()
+        {
+            var actual = "dd";
+
+            Assert.That(actual, Matches.ToSnapshot);
+        }
+    }
+}
---
+++ src/Snapshooter.NUnit/NUnitThat.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework.Constraints;
+
+namespace Snapshooter.NUnit
+{
+    class NUnitThat
+    {
+    }
+
+    public class MatchSnapshotConstraint : Constraint
+    {
+        public override ConstraintResult ApplyTo<TActual>(TActual actual)
+        {
+            SnapResult snapResult;
+            if (_snapshotId != null)
+            {
+                var snapper = NUnitSnapperFactory.GetNUnitSnapper();
+                snapResult = snapper.MatchSnapshot(actual, _snapshotId);
+            }
+            else
+            {
+                snapResult = MatchSnapshot(actual);
+            }
+
+            return new ConstraintResult(this, actual, snapResult);
+        }
+    }
+}
