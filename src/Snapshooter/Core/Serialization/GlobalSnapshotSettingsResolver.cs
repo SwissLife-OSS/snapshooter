fbfd5159a8d1daa65239a606c2eb5fc184db8a21
Jan Paolo Go <go.janpaolo@gmail.com>
2021-02-27 18:07:05 -0600

    Catch ReflectionTypeLoadException on Assembly.GetTypes

--- src/Snapshooter/Core/Serialization/GlobalSnapshotSettingsResolver.cs
+++ src/Snapshooter/Core/Serialization/GlobalSnapshotSettingsResolver.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 
 namespace Snapshooter.Core.Serialization
 {
@@ -11,10 +12,20 @@ namespace Snapshooter.Core.Serialization
             Type type = typeof(SnapshotSerializerSettings);
 
             var typesExtendingSettings = AppDomain.CurrentDomain.GetAssemblies()
-                .SelectMany(s => s.GetTypes())
-                .Where(p => 
-                    type.IsAssignableFrom(p) && 
-                    p.IsClass && 
+                .SelectMany(s =>
+                {
+                    try
+                    {
+                        return s.GetTypes();
+                    }
+                    catch (ReflectionTypeLoadException e)
+                    {
+                        return e.Types;
+                    }
+                })
+                .Where(p =>
+                    type.IsAssignableFrom(p) &&
+                    p.IsClass &&
                     p.GetConstructor(Type.EmptyTypes) != null)
                 .ToList();
 
