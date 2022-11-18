b93ac3d19eb4ce57ac3a27aeca5d8489fd4d7227
Normen Scheiber <nscheibe@live.com>
2022-11-18 15:35:54 +0100

    Added some comments.

--- src/Snapshooter/Core/MatchOperators/FieldMatchOperator.cs
+++ src/Snapshooter/Core/MatchOperators/FieldMatchOperator.cs
@@ -4,12 +4,31 @@ using Newtonsoft.Json.Linq;
 
 namespace Snapshooter.Core;
 
+/// <summary>
+/// Base class of the match operators.
+/// </summary>
 public abstract class FieldMatchOperator
 {
+    /// <summary>
+    /// Defines if the field match operator will format
+    /// the snapshot.
+    /// </summary>
+    /// <returns>True if the snapshot will be formatted, otherwise False</returns>
     public abstract bool HasFormatAction();
 
+    /// <summary>
+    /// Formats the specified fields of the snapshot.
+    /// </summary>
+    /// <param name="snapshotData">The entire snapshot.</param>
     public abstract void FormatFields(JToken snapshotData);
 
+    /// <summary>
+    /// Compares the specified match fields of the
+    /// current snapshot with the original snapshot.
+    /// </summary>
+    /// <param name="snapshotData">The current snapshot.</param>
+    /// <param name="expectedSnapshotData">The original snapshot.</param>
+    /// <returns></returns>
     public abstract FieldOption ExecuteMatch(
         JToken snapshotData, JToken expectedSnapshotData);
     
--- src/Snapshooter/Core/MatchOperators/IncludeMatchOperator.cs
+++ src/Snapshooter/Core/MatchOperators/IncludeMatchOperator.cs
@@ -4,17 +4,31 @@ using Newtonsoft.Json.Linq;
 
 namespace Snapshooter.Core;
 
+/// <summary>
+/// This field match operator can include some specific
+/// fields of a snapshot. Only the specified fields will
+/// be included within the snapshot. All other fields will
+/// be removed/excluded from the snapshot.
+/// </summary>
 public class IncludeMatchOperator : FieldMatchOperator
 {
     private readonly List<string> _fieldsPaths;
 
+    /// <summary>
+    /// Creates a new instance of the <see cref="IncludeMatchOperator"/>
+    /// </summary>
     public IncludeMatchOperator()
     {
         _fieldsPaths = new List<string>();
     }
 
+    /// <inheritdoc/>
     public override bool HasFormatAction() => true;
 
+    /// <summary>
+    /// Adds a path of a field to include.
+    /// </summary>
+    /// <param name="fieldPath">The path of a field to include.</param>
     public void AddFieldPath(string fieldPath)
     {
         if(!_fieldsPaths.Contains(fieldPath))
@@ -23,6 +37,7 @@ public class IncludeMatchOperator : FieldMatchOperator
         }
     }
 
+    /// <inheritdoc/>
     public override void FormatFields(JToken snapshotData)
     {
         FieldOption fieldOption = new FieldOption(snapshotData);
@@ -41,6 +56,7 @@ public class IncludeMatchOperator : FieldMatchOperator
         FilterIncludedFieldsOnly(snapshotData, includedFields);
     }
 
+    /// <inheritdoc/>
     public override FieldOption ExecuteMatch(
         JToken snapshotData,
         JToken expectedSnapshotData)
