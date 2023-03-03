5406eae1cf1aafcc13818d1f342fbe869d584395
Tim Holzherr <Tim.Holzherr@swisslife.ch>
2023-03-03 13:16:29 +0100

    remove warnings about missing editor hints in FluentAssertionsExtension.cs

--- src/Snapshooter/Extensions/FluentAssertionsExtension.cs
+++ src/Snapshooter/Extensions/FluentAssertionsExtension.cs
@@ -3,15 +3,25 @@ using FluentAssertions.Primitives;
 // Extension of Fluent assertion to not loose type information of asserted object
 namespace FluentAssertions;
 
+/// <summary>
+/// Contains a number of methods to assert that an <see cref="object"/> is in the expected state.
+/// </summary>
 public class TypedAssertions<T> : ObjectAssertions<T, TypedAssertions<T>>
 {
-    public TypedAssertions(T value) : base(value)
+    internal TypedAssertions(T value) : base(value)
     {
     }
 }
 
-public static class Extensions
+/// <summary>
+/// Contains extension methods for custom assertions in unit tests.
+/// </summary>
+public static class AssertionExtensions
 {
+    /// <summary>
+    /// Returns an <see cref="TypedAssertions{T}"/> object that can be used to assert the
+    /// current <see cref="object"/>.
+    /// </summary>
     public static TypedAssertions<TSubject> Should<TSubject>(this TSubject actualValue)
     {
         return new TypedAssertions<TSubject>(actualValue);
