72cf492f15e2b5502979dc44b20aa42a2fa1b1b9
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-12-20 11:15:42 +0000

    TUnit Support (#202)
    * TUnit Support
    
    * Update TUnit
    
    ---------
    
    Co-authored-by: Tom Longhurst <130671542+msm-tomlonghurst@users.noreply.github.com>
    Co-authored-by: PascalSenn <senn.pasc@gmail.com>

---
+++ src/Snapshooter.TUnit/TUnitAssert.cs
@@ -0,0 +1,26 @@
+﻿using Snapshooter.Core;
+using TUnit.Assertions.Extensions;
+using TAssert = TUnit.Assertions.Assert;
+
+namespace Snapshooter.TUnit
+{
+    /// <summary>
+    /// The NunitAssert instance compares two strings with the TUnit assert utility.
+    /// </summary>
+    public class TUnitAssert : IAssert
+    {
+        /// <summary>
+        /// Asserts the expected snapshot and the actual snapshot 
+        /// with the TUnit assert utility.
+        /// </summary>
+        /// <param name="expectedSnapshot">The expected snapshot.</param>
+        /// <param name="actualSnapshot">The actual snapshot.</param>
+        public void Assert(string expectedSnapshot, string actualSnapshot)
+        {
+            // TUnit assertions use an async syntax but this interface is restricted to synchronous calls
+#pragma warning disable TUnitAssertions0002
+            TAssert.That(actualSnapshot).IsEqualTo(expectedSnapshot).GetAwaiter().GetResult();
+#pragma warning restore TUnitAssertions0002
+        }
+    }
+}
